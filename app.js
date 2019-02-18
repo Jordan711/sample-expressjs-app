5bea1bc1957e5f254c7ef5307c0c128f83f0aa37
Jordan Feng <jordan.feng@hotmail.com>
2019-02-17 23:49:49 -0500

    Add form for range of number

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const expHandlebars = require('express-handlebars');
 var port = 8080;
 
 app.engine('handlebars', expHandlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', function(req, res) {
   res.render('index', {pageTitle: 'HOMEPAGE'});
@@ -14,7 +17,29 @@ app.get('/', function(req, res) {
 
 app.get('/lucky-number', function(req, res){
   var number = Math.floor(Math.random() * 1000);
-  res.render('index', {pageTitle: number});
+  res.render('lucky-number', {
+    pageTitle: 'Lucky Number',
+    number: number
+  });
+});
+
+app.post('/lucky-number', function(req, res){
+  if (isNaN(req.body.lower) || isNaN(req.body.upper)) {
+    res.render('lucky-number', {
+      pageTitle: 'Lucky Number',
+      errorMessage: 'Please Enter valid numbers :((('
+    });
+  } else {
+    var lower = parseInt(req.body.lower);
+    var upper = parseInt(req.body.upper);
+    var number = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+
+    res.render('lucky-number', {
+      pageTitle: 'Lucky Number',
+      number: number
+    });
+  }
+
 });
 
 app.get('*', function(req, res){
