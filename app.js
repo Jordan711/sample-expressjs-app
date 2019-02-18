458de9afdf4210e198152ee162a85f53d02d0d64
Jordan Feng <jord.feng@mail.utoronto.ca>
2019-02-17 19:30:15 -0500

    Added basic views and routes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var http = require('http');
 const express = require('express');
 const app = express();
+
+const expHandlebars = require('express-handlebars');
 var port = 8080;
 
+app.engine('handlebars', expHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index', {pageTitle: 'HOMEPAGE'});
+});
+
+app.get('/lucky-number', function(req, res){
+  var number = Math.floor(Math.random() * 1000);
+  res.render('index', {pageTitle: number});
+});
+
+app.get('*', function(req, res){
+  res.render('404');
 });
 
 app.listen(port, () => console.log('Listening on port: ' + port));
